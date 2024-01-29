3d10bb470a4b78bf15215876923a81d976a5cc59
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-30 00:01:15 +0200

    feat: cors improvements

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,14 @@ import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    optionsSuccessStatus: 200,
+    methods: '*',
+    maxAge: 1000 * 60 * 60 * 24 * 14,
+  });
   await app.listen(3000);
 }
 
