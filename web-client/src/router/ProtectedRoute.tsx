a26cb6bda672835e453df281cbb36ecf634fd843
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-30 14:56:49 +0200

    feat: add token check on protected routes

--- web-client/src/router/ProtectedRoute.tsx
+++ web-client/src/router/ProtectedRoute.tsx
@@ -1,13 +1,13 @@
 import {Outlet, useNavigate} from 'react-router-dom';
 import {useEffect} from 'react';
+import {CookieService} from '@/services/CookieService.ts';
 
 export default function ProtectedRoute() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    //TODO auth check
-    const isAuthenticated = true;
-    if (!isAuthenticated) {
+    const token = new CookieService().getToken();
+    if (!token) {
       navigate('/login');
     }
   }, [navigate]);
