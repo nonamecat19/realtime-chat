b64efa0324d3982feffb87dcd41d8cbfc44a286f
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-31 23:41:26 +0200

    feat: add limit in 15 seconds to messages

--- server/src/modules/chat/services/chat.service.ts
+++ server/src/modules/chat/services/chat.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, Logger, NotFoundException} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {ChatMessage} from '../../../../db/entities/chatMessage.entity';
@@ -8,18 +8,24 @@ import {ChatServer} from '../types/chatEvents.types';
 import Redis from 'ioredis';
 import {InjectRedis} from '@nestjs-modules/ioredis';
 import {JwtData} from '../../shared/types/jwt.types';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class ChatService {
   logger = new Logger(ChatService.name);
-
+  private readonly MINIMUM_TIME_BETWEEN_MESSAGES: number;
   constructor(
     @InjectRepository(ChatMessage)
     private readonly chatMessageRepository: Repository<ChatMessage>,
     private readonly usersService: UsersService,
     @InjectRedis()
-    private readonly redis: Redis
-  ) {}
+    private readonly redis: Redis,
+    private readonly configService: ConfigService
+  ) {
+    this.MINIMUM_TIME_BETWEEN_MESSAGES = configService.getOrThrow<number>(
+      'chat.minimumTimeBetweenMessage'
+    );
+  }
   async getLastMessages() {
     return await this.chatMessageRepository.find({
       relations: {
@@ -30,10 +36,21 @@ export class ChatService {
   }
 
   async sendMessage(userId: number, message: string, client: Socket) {
+    const lastMessageTime = await this.redis.get(`lastMessage-${client.id}`);
+    console.log(`lastMessage-${client.id}`);
+    if (lastMessageTime) {
+      const lastMessageDate = new Date(lastMessageTime);
+      const currentDate = new Date();
+      const differenceInMs = currentDate.getTime() - lastMessageDate.getTime();
+      if (differenceInMs < this.MINIMUM_TIME_BETWEEN_MESSAGES) {
+        return client.emit('error', {status: 429, message: 'Too many requests'});
+      }
+    }
+
     const user = await this.usersService.findOneById(userId);
     if (!user) {
       this.logger.error(`User not found. UserId: ${userId}`);
-      throw new NotFoundException('User not found');
+      return client.emit('error', {status: 404, message: 'User not found'});
     }
     if (user.isMuted) {
       return client.emit('error', {status: 400, message: 'User is muted'});
@@ -48,6 +65,7 @@ export class ChatService {
     const result = await this.chatMessageRepository.save(newMessage);
     client.broadcast.emit('receiveMessage', result);
     client.emit('receiveMessage', result);
+    await this.redis.set(`lastMessage-${client.id}`, new Date().toString());
   }
 
   async broadcastOnlineStatus(server: ChatServer) {
--- server/src/modules/shared/config/index.ts
+++ server/src/modules/shared/config/index.ts
@@ -3,5 +3,13 @@ import AppConfig from './app.config';
 import DatabaseConfig from './database.config';
 import JwtConfig from './jwt.config';
 import RedisConfig from './redis.config';
+import ChatConfig from './chat.config';
 
-export const AppConfigs = [AppConfig, DatabaseConfig, JwtConfig, RedisConfig, CookieConfig];
+export const AppConfigs = [
+  AppConfig,
+  DatabaseConfig,
+  JwtConfig,
+  RedisConfig,
+  CookieConfig,
+  ChatConfig,
+];
