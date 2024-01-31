ae69a0ceaeecb9fcbdd23eae78463a4cdaa6fc4b
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-31 16:53:26 +0200

    refactor: business logic from gateway to service

--- server/src/modules/users/gateways/users.gateway.ts
+++ server/src/modules/users/gateways/users.gateway.ts
@@ -7,46 +7,29 @@ import {
 } from '@nestjs/websockets';
 import {UsersService} from '../services/users.service';
 import {UpdateUserDto} from '../dto/update-user.dto';
-import {Logger, UseFilters, UseGuards, UsePipes, ValidationPipe} from '@nestjs/common';
+import {UseFilters, UseGuards, UsePipes, ValidationPipe} from '@nestjs/common';
 import {BaseWebSocketGateway} from '../../shared/decorators/base-ws-gateway.decorator';
 import {ReservedOrUserListener} from 'socket.io/dist/typed-events';
 import {UsersServer} from '../types/usersEvents.types';
 import {WsJwtGuard} from '../../shared/guards/ws-jwt.guard';
 import {WsExceptionFilter} from '../../shared/filters/ws-validation.filter';
-import Redis from 'ioredis';
-import {InjectRedis} from '@nestjs-modules/ioredis';
 
 @BaseWebSocketGateway()
 @UseGuards(WsJwtGuard)
 @UsePipes(ValidationPipe)
 @UseFilters(WsExceptionFilter)
 export class UsersGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  logger = new Logger(UsersGateway.name);
-
-  constructor(
-    private readonly usersService: UsersService,
-    @InjectRedis()
-    private readonly redis: Redis
-  ) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @WebSocketServer()
   server: UsersServer;
 
   async handleConnection(client: ReservedOrUserListener<any, any, any>) {
-    try {
-      this.logger.log(`Connected: ${client.id}`);
-    } catch (e) {
-      this.logger.error(`handleConnection: ${e.message}`);
-    }
+    await this.usersService.handleConnection(client);
   }
 
   async handleDisconnect(client: ReservedOrUserListener<any, any, any>) {
-    try {
-      await this.redis.del(client.id);
-      this.logger.log(`Disconnected: ${client.id}`);
-    } catch (e) {
-      this.logger.error(`handleDisconnect: ${e.message}`);
-    }
+    await this.usersService.handleDisconnect(client);
   }
 
   @SubscribeMessage('findAllUsers')
--- server/src/modules/users/services/users.service.ts
+++ server/src/modules/users/services/users.service.ts
@@ -1,16 +1,22 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {UpdateUserDto} from '../dto/update-user.dto';
 import {InjectRepository} from '@nestjs/typeorm';
 import {RoleEnum, User} from '../../../../db/entities/user.entity';
 import {Repository} from 'typeorm';
 import {getRandomHex} from '../../shared/utils/colors.utils';
 import * as bcrypt from 'bcrypt';
+import {ReservedOrUserListener} from 'socket.io/dist/typed-events';
+import {InjectRedis} from '@nestjs-modules/ioredis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class UsersService {
+  logger = new Logger(UsersService.name);
   constructor(
     @InjectRepository(User)
-    private readonly usersRepository: Repository<User>
+    private readonly usersRepository: Repository<User>,
+    @InjectRedis()
+    private readonly redis: Redis
   ) {}
   public async create(login: string, password: string) {
     const saltOrRounds = 10;
@@ -42,5 +48,22 @@ export class UsersService {
 
   public remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async handleConnection(client: ReservedOrUserListener<any, any, any>) {
+    try {
+      this.logger.log(`Connected: ${client.id}`);
+    } catch (e) {
+      this.logger.error(`handleConnection: ${e.message}`);
+    }
+  }
+
+  async handleDisconnect(client: ReservedOrUserListener<any, any, any>) {
+    try {
+      await this.redis.del(client.id);
+      this.logger.log(`Disconnected: ${client.id}`);
+    } catch (e) {
+      this.logger.error(`handleDisconnect: ${e.message}`);
+    }
   }
 }
