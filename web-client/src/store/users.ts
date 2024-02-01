6403197e94a870be3582cb1fea0cc1e43d620703
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-01 10:58:42 +0200

    feat: add user data persists

--- web-client/src/store/users.ts
+++ web-client/src/store/users.ts
@@ -1,4 +1,26 @@
-import {atom} from 'jotai';
 import {LoginUserData} from '@/types/user.types.ts';
+import {atom, WritableAtom} from 'jotai';
 
-export const userDataAtom = atom<LoginUserData | null>(null);
+export const userDataAtom = atomWithLocalStorage<LoginUserData | null>('userData', null);
+
+function atomWithLocalStorage<T = unknown>(
+  key: string,
+  initialValue: T
+): WritableAtom<T, [update: T], void> {
+  const getInitialValue = () => {
+    const item = localStorage.getItem(key);
+    if (item !== null) {
+      return JSON.parse(item);
+    }
+    return initialValue;
+  };
+  const baseAtom = atom(getInitialValue());
+  return atom(
+    get => get(baseAtom),
+    (get, set, update) => {
+      const nextValue = typeof update === 'function' ? update(get(baseAtom)) : update;
+      set(baseAtom, nextValue);
+      localStorage.setItem(key, JSON.stringify(nextValue));
+    }
+  );
+}
