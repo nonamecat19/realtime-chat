dbe20c58e4cb528509dd9ca75b1878c954a8e203
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-01 13:16:42 +0200

    feat(server): add user login validation

--- server/src/modules/auth/dto/login.dto.ts
+++ server/src/modules/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
-import {IsString} from 'class-validator';
+import {IsString, Matches, MinLength} from 'class-validator';
 
 export class LoginDto {
+  @MinLength(3, {message: 'Nickname should be at least 3 characters'})
+  @Matches(/^[a-zA-Z0-9]+$/, {message: 'Nickname should not contain special symbols'})
   @IsString()
   login: string;
 
+  @MinLength(8, {message: 'Password should be at least 8 characters'})
   @IsString()
   password: string;
 }
