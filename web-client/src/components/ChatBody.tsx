3e0831e0c55666490d2de4b17aef0390b383a205
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-01 12:43:45 +0200

    feat(web-client): scroll to bottom on message receive

--- web-client/src/components/ChatBody.tsx
+++ web-client/src/components/ChatBody.tsx
@@ -1,6 +1,6 @@
 import MessageBlock from '@/components/MessageBlock.tsx';
 import {MappedChatMessage} from '@/types/chat.types.ts';
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import {SocketEvent, useSocketEvents} from '@/hooks/useSocketEvents.ts';
 import {SocketApi} from '@/api/socket.ts';
 import {useAtomValue, useSetAtom} from 'jotai';
@@ -11,6 +11,8 @@ export function ChatBody() {
   const setOnline = useSetAtom(onlineAtom);
   const setMessages = useSetAtom(messagesAtom);
   const mappedMessages = useAtomValue(mappedMessagesAtom);
+
+  const messagesContainerRef = useRef<HTMLUListElement>(null);
 
   useConnectSocket();
 
@@ -32,6 +34,13 @@ export function ChatBody() {
       name: 'receiveMessage',
       handler: data => {
         setMessages(prev => [...prev, data]);
+        setTimeout(() => {
+          const container = messagesContainerRef.current;
+          if (!container) {
+            return;
+          }
+          container.scrollTop = container.scrollHeight;
+        }, 200);
       },
     },
   ];
@@ -39,7 +48,10 @@ export function ChatBody() {
   useSocketEvents(events);
 
   return (
-    <ul className="flex flex-col gap-5 mx-5 h-[calc(100vh-145px)] sm:h-[calc(100vh-125px)] overflow-x-scroll mb-3 pr-5">
+    <ul
+      className="flex flex-col gap-5 mx-5 h-[calc(100vh-145px)] sm:h-[calc(100vh-125px)] overflow-x-scroll mb-3 pr-5 scroll-smooth"
+      ref={messagesContainerRef}
+    >
       {mappedMessages.map(el => (
         <MessageBlock {...el} key={el.id} />
       ))}
