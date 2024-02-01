570d488b63f9c60b20837a133150111fad38e7d0
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-01 12:53:26 +0200

    feat(web-client): disable button on timeout and empty message

--- web-client/src/components/ChatFooter.tsx
+++ web-client/src/components/ChatFooter.tsx
@@ -2,19 +2,32 @@ import {Input} from '@/components/ui/input.tsx';
 import {Button} from '@/components/ui/button.tsx';
 import {SocketApi} from '@/api/socket.ts';
 import {useState} from 'react';
-import {Forward} from 'lucide-react';
+import {Forward, Timer} from 'lucide-react';
+import {useConnectSocket} from '@/hooks/useConnectSocket.ts';
 
 export default function ChatFooter() {
   const [userMessage, setUserMessage] = useState<string>('');
+  useConnectSocket();
+  const [onTimeout, setOnTimeout] = useState<boolean>(false);
+
   async function sendMessageHandle() {
     SocketApi.socket?.emit('sendMessage', {message: userMessage});
+    setUserMessage('');
+    setOnTimeout(true);
+    setTimeout(() => {
+      setOnTimeout(false);
+    }, 15_000);
   }
 
   return (
     <footer className="flex py-2 px-5 gap-3 h-16">
       <Input value={userMessage} onChange={e => setUserMessage(e.target.value)} className="h-12" />
-      <Button onClick={sendMessageHandle} className="h-12">
-        <Forward />
+      <Button
+        disabled={userMessage.length === 0 || onTimeout}
+        onClick={sendMessageHandle}
+        className="h-12"
+      >
+        {onTimeout ? <Timer /> : <Forward />}
       </Button>
     </footer>
   );
