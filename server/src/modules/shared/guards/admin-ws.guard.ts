e58b90e472b7fb7539ef6c8a4409f03062f8229c
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-02 15:50:15 +0200

    refactor(server): hardcode to enums

--- server/src/modules/shared/guards/admin-ws.guard.ts
+++ server/src/modules/shared/guards/admin-ws.guard.ts
@@ -1,13 +1,14 @@
 import {CanActivate, ExecutionContext, Injectable, Logger} from '@nestjs/common';
 import {Socket} from 'socket.io';
+import {RoleEnum} from '../../../../db/entities/user.entity';
 
 @Injectable()
 export class AdminWsGuard implements CanActivate {
   logger = new Logger(AdminWsGuard.name);
   canActivate(context: ExecutionContext): boolean {
     const client: Socket = context.switchToWs().getClient();
-    const role = client?.data?.user?.user?.role ?? 'USER';
-    if (role === 'ADMIN') {
+    const role = client?.data?.user?.user?.role ?? RoleEnum.USER;
+    if (role === RoleEnum.ADMIN) {
       return true;
     }
     this.logger.debug('Permission error');
